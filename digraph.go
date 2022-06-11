1b8486f76a38f45e6394fab9adf6e07508d4954c
Lambels <patrick.arvatu@yahoo.com>
2022-06-11 15:11:26 +0300

    implemented furthermore digraph

--- digraph.go
+++ digraph.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"io"
+	"strings"
 
 	"github.com/xuri/excelize/v2"
 )
 
+var validRunes = [...]bool{
+	'+': true,
+	'-': true,
+	'*': true,
+	'/': true,
+	'(': true,
+	')': true,
+}
+
 type cell struct {
-	id        uint
-	lowLink   int
-	val       string
-	isFormula bool
+	id      uint
+	y       uint
+	lowLink int
+	val     string
 }
 
 type digraph struct {
-	formulas []*cell
-	cells    []*cell
+	formulas map[uint]*cell
+	cells    map[uint]*cell
 	edges    map[cell][]*cell
 }
 
@@ -30,6 +40,11 @@ func newGraph(from io.Reader) (*digraph, error) {
 		return nil, err
 	}
 
+	graph := &digraph{
+		formulas: make(map[uint]*cell),
+		cells:    make(map[uint]*cell),
+		edges:    make(map[cell][]*cell),
+	}
 	var colx, rowx int
 	for _, row := range rows {
 		rowx++
@@ -49,17 +64,60 @@ func newGraph(from io.Reader) (*digraph, error) {
 				return nil, err
 			}
 
-			c := &cell{
-				id:        concat(uint(colx), uint(rowx)),
-				val:       axis,
-				isFormula: formula != "",
+			val, err := f.GetCellValue("Sheet1", axis)
+			if err != nil {
+				return nil, err
 			}
 
+			c := &cell{
+				id:  concat(uint(colx), uint(rowx)),
+				y:   uint(rowx),
+				val: val,
+			}
+
+			graph.addCell(c, formula)
 		}
 		colx = 0
 	}
 
 	return nil, nil
+}
+
+func (d *digraph) addCell(c *cell, formula string) error {
+	if formula != "" {
+		// add cell.
+		d.formulas[c.id] = c
+
+		axis := digestFormula(formula)
+
+		// TODO: attaching algorithm
+		for _, pos := range axis {
+			x, y, err := excelize.CellNameToCoordinates(pos)
+			if err != nil {
+				return err
+			}
+
+		}
+	}
+}
+
+func digestFormula(formula string) []string {
+	s := strings.FieldsFunc(formula, func(r rune) bool {
+		i := int(r)
+		if i < len(validRunes) {
+			return validRunes[i]
+		}
+		return false
+	})
+
+	// iterate backwards to not worry about index shifting.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i][0] >= 48 && s[i][0] <= 57 { // check if byte is number.
+			s = append(s[:i], s[i+1:]...)
+		}
+	}
+
+	return s
 }
 
 func concat(x, y uint) uint {
@@ -68,4 +126,15 @@ func concat(x, y uint) uint {
 		mul *= 10
 	}
 	return x*mul + y
+}
+
+func unConcat(xy, y uint) uint {
+	var mul uint = 10
+	for y >= mul {
+		mul *= 10
+	}
+
+	dif := xy - y
+
+	return dif / mul
 }
